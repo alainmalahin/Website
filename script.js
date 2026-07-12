05d5456d0de2b045dca04bf0e58611cc63ae3762
alainmalahin <alainmalahin@gmail.com>
2026-07-12 18:59:34 +0200

    Actualizar script.js

--- script.js
+++ script.js
@@ -71,3 +71,20 @@ if (cinematicEntry && doorEnter) {
 }
 
 });
+// VIDEO DE ENTRADA
+
+const entranceVideo = document.getElementById("entrance-video");
+const videoEntry = document.getElementById("video-entry");
+const hero = document.getElementById("hero");
+
+if (entranceVideo && hero) {
+
+    entranceVideo.addEventListener("ended", () => {
+
+        entranceVideo.pause();
+
+        hero.classList.add("show");
+
+    });
+
+}
