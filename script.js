0b40ce571e1d45c30b1690ee4eb4d6109224c331
alainmalahin <alainmalahin@gmail.com>
2026-07-14 09:40:27 +0200

    Actualizar script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     entranceVideo.addEventListener("timeupdate", () => {
 
-        if (entranceVideo.currentTime >= 2,5) {
+        if (entranceVideo.currentTime >= 2,8) {
             hero.classList.add("show");
         }
 
